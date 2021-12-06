8e799e8200b6944cf558d908652d2e3cd9f37b01
황일영 <codeclz2021@gmail.com>
2021-12-06 16:48:18 +0900

    MyList

--- src/com/javaex/ex03/MyList.java
+++ src/com/javaex/ex03/MyList.java
@@ -1,14 +1,17 @@
 package com.javaex.ex03;
 
-public class MyList {
+import java.util.Arrays;
+
+//T  --> Point     Circle
+public class MyList<T> {
 	
 	//필드
-	private Object[] oArray;
+	private T[] oArray;
 	private int crtPos;
 	
 	//생성자
 	public MyList() {
-		this.oArray = new Object[3];
+		this.oArray = (T[])new Object[3];
 		this.crtPos = 0;
 	}
 	
@@ -16,20 +19,26 @@ public class MyList {
 	//메소드 g/s
 	
 	//메소드 일반
-	public void add(Object o) {
+	public void add(T o) {
+		
 		oArray[crtPos] = o;
 		crtPos++;
 	}
 	
-	public Object get(int index) {
-		Object result = oArray[index];
+	public T get(int index) {
+		T result = oArray[index];
 		return result;
 	}
 	
 	public int size() {
 		return crtPos;
 	}
-	
+
+
+	@Override
+	public String toString() {
+		return "MyList [oArray=" + Arrays.toString(oArray) + ", crtPos=" + crtPos + "]";
+	}
 	
 
 }
--- src/com/javaex/ex03/MyListApp.java
+++ src/com/javaex/ex03/MyListApp.java
@@ -1,26 +1,36 @@
 package com.javaex.ex03;
 
-import com.javaex.ex01.PointList;
-import com.javaex.ex02.CircleList;
 
 public class MyListApp {
 
 	public static void main(String[] args) {
 		
-		Circle c01 = new Circle(5);
-		Circle c02 = new Circle(19);
-		
-		CircleList cList = new CircleList();
-		cList.add(c01);
-		
-		
+		//도형생성
 		Point p01 = new Point(4,7);
+		Point p02 = new Point(8,17);
 		
-		PointList pList = new PointList();
+		Circle c01 = new Circle(5);
+		
+		
+		
+		//포인트(점) 관리
+		MyList<Point> pList = new MyList<Point>();  //Point 만 관리
 		pList.add(p01);
+		pList.add(p02);
+		
+		System.out.println(pList.toString());
+	
+		//Circle(원) 관리
+		MyList<Circle> cList = new MyList<Circle>();  //Circle만 관리
+		cList.add(c01);
+		//cList.add(p01);   // <-- 제네릭으로 정의한 클래스 이외의 다른값은 오류발생
+		
+		System.out.println(cList.get(0));  // <--- 다운캐스팅 하지 않아도 됨
 		
 		
-		
+		System.out.println(cList.toString());
+				
+	
 		
 
 	}
